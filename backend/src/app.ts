3bbd29f22f6fac0a325efb1ed7af4c1984cc9ca8
chen <23054115+cwkang1998@users.noreply.github.com>
2026-02-17 16:24:04 +0800

    feat: modify / to not send raw error
    / act as a healthcheck endpoint, and we don't really want to send the
    raw error traces to the user/service calling us. Change it to provide a
    simple status payload to the caller, whilst still logging the error traces.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,10 @@
 import cors from "cors";
-import express, { type Express, type Request, type Response } from "express";
+import express, {
+	type Express,
+	type NextFunction,
+	type Request,
+	type Response,
+} from "express";
 import morgan from "morgan";
 import type { Pool } from "mysql2/promise";
 import {
@@ -8,6 +13,7 @@ import {
 	marketTvlByMarketIdHandler,
 	marketTvlHandler,
 } from "./handlers";
+import { HttpError } from "./http";
 import { MarketService } from "./services";
 
 export const createApp = (pool: Pool) => {
@@ -18,23 +24,30 @@ export const createApp = (pool: Pool) => {
 	app.use(cors());
 	app.use(express.json());
 
-	const testDbConnection = async () => {
+	const testDbConnection = async (): Promise<boolean> => {
 		try {
 			const connection = await pool.getConnection();
-
 			connection.release();
-			return "Database connection established successfully";
 		} catch (error) {
-			return `Error connecting to database: ${error}`;
+			console.error(`Error connecting to database: ${error}`);
+			return false;
 		}
+		return true;
 	};
 
 	// Services
 	const marketService = new MarketService(pool);
 
-	app.get("/", async (req: Request, res: Response) => {
-		const response = await testDbConnection();
-		res.json({ response });
+	// Healthcheck api
+	app.get("/", async (_req: Request, res: Response) => {
+		const isDbOk = await testDbConnection();
+		let statusCodeToReturn = 200;
+
+		if (!isDbOk) {
+			statusCodeToReturn = 503;
+		}
+
+		res.status(statusCodeToReturn).json({ db_alive: isDbOk });
 	});
 
 	app.get("/tvl", marketTvlHandler(marketService));
@@ -44,6 +57,12 @@ export const createApp = (pool: Pool) => {
 		"/liquidity/:marketId",
 		marketLiquidityByMarketIdHandler(marketService),
 	);
+
+	// Catch all middleware
+	app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+		console.error(err);
+		return res.status(500).json(HttpError.SERVER_UNEXPECTED_ERROR_RESPONSE);
+	});
 
 	return app;
 };
